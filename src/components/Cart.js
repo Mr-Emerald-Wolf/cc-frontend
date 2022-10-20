68b1cdae972d935c2c4f28320eaa38a020515200
Mr-Emerald-Wolf <shivamsharma2002@gmail.com>
2022-10-21 04:23:09 +0530

    Minor UI Changes

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useStore } from '../store'
 import CartItem from './CartItem'
+import { Link } from 'react-router-dom';
 
 
 export default function Cart() {
@@ -30,11 +31,11 @@ export default function Cart() {
                         
                           
 
-                        <a href="/#" class="flex font-semibold text-indigo-600 text-sm mt-10">
+                        <Link to="/" class="flex font-semibold text-indigo-600 text-sm mt-10">
 
                             <svg class="fill-current mr-2 text-indigo-600 w-4" viewBox="0 0 448 512"><path d="M134.059 296H436c6.627 0 12-5.373 12-12v-56c0-6.627-5.373-12-12-12H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.569 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296z" /></svg>
                             Continue Shopping
-                        </a>
+                        </Link>
                     </div>
 
                     <div id="summary" class="w-1/4 px-8 py-10">
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -37,7 +37,7 @@ const Dashboard = () => {
     //         return Promise.reject(error);
     //     }
     // );
-    
+
     const getUsers = async () => {
         var config = {
             method: 'get',
@@ -62,41 +62,42 @@ const Dashboard = () => {
     return (
         <>
 
-            {<div class="overflow-hidden bg-white shadow sm:rounded-lg">
-                <div class="px-4 py-5 sm:px-6">
-                    <h3 class="text-lg font-medium leading-6 text-gray-900">Profile Information</h3>
-                    <p class="mt-1 max-w-2xl text-sm text-gray-500">Personal User Details</p>
-                </div>
-                <div class="border-t border-gray-200">
-                    <dl>
-                        <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                            <dt class="text-sm font-medium text-gray-500">Full name</dt>
-                            <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{!loading && data.firstName + " " + data.lastName}</dd>
-                        </div>
-                        <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                            <dt class="text-sm font-medium text-gray-500">Role</dt>
-                            <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{data.role}</dd>
-                        </div>
-                        <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                            <dt class="text-sm font-medium text-gray-500">Email address</dt>
-                            <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{data.email}</dd>
-                        </div>
-                        <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                            <dt class="text-sm font-medium text-gray-500">Address</dt>
-                            <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim incididunt cillum culpa consequat. Excepteur qui ipsum aliquip consequat sint. Sit id mollit nulla mollit nostrud in ea officia proident. Irure nostrud pariatur mollit ad adipisicing reprehenderit deserunt qui eu.</dd>
-                        </div>
-                    </dl>
-
-                </div>
-
-            </div>
-            } <button type="submit" onClick={() => { navigate('/admin') }} class="m-3 group relative flex mx-auto justify-center rounded-md border border-transparent bg-pink-800 py-2 px-4 text-sm font-medium text-white hover:bg-lavender focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+            {<div class="bg-slate-100 p-2 min-h-screen">
+                <section className="p-2 bg-white shadow sm:rounded-lg">
+                    <div class="px-4 py-5 sm:px-6">
+                        <h3 class="text-lg font-medium leading-6 text-gray-900">Profile Information</h3>
+                        <p class="mt-1 max-w-2xl text-sm text-gray-500">Personal User Details</p>
+                    </div>
+                    <div class="border-t border-gray-200">
+                        <dl>
+                            <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                <dt class="text-sm font-medium text-gray-500">Full name</dt>
+                                <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{!loading && data.firstName + " " + data.lastName}</dd>
+                            </div>
+                            <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                <dt class="text-sm font-medium text-gray-500">Role</dt>
+                                <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{data.role}</dd>
+                            </div>
+                            <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                <dt class="text-sm font-medium text-gray-500">Email address</dt>
+                                <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{data.email}</dd>
+                            </div>
+                            <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                <dt class="text-sm font-medium text-gray-500">Address</dt>
+                                <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim incididunt cillum culpa consequat. Excepteur qui ipsum aliquip consequat sint. Sit id mollit nulla mollit nostrud in ea officia proident. Irure nostrud pariatur mollit ad adipisicing reprehenderit deserunt qui eu.</dd>
+                            </div>
+                        </dl>
+                    </div>
+                </section>
+                <button type="submit" onClick={() => { navigate('/admin') }} class="m-3 group relative flex mx-auto justify-center rounded-md border border-transparent bg-pink-800 py-2 px-4 text-sm font-medium text-white hover:bg-lavender focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
                 <span class="absolute inset-y-0 left-0 flex items-center pl-3">
                     {/* <!-- Heroicon name: mini/lock-closed --> */}
 
                 </span>
                 Admin Dashboard
             </button>
+            </div>
+            } 
         </>
     )
 }
